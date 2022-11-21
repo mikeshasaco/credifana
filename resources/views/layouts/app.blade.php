cc477c925ee2b9bf9859f18f20c9c41ceb0b1602
vajirali530 <vajirali530@gmail.com>
2022-11-21 19:30:29 +0530

    title and description updated

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,10 +19,11 @@
     <meta name="msapplication-TileColor" content="#ffffff">
     <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
     <meta name="theme-color" content="#ffffff">
+    <meta name="description" content="Automation Tool for Real Estate Analysis and property investments, to provide real-time metrics for properties gross revenue and net profit.">
     <link rel="stylesheet" href="{{ asset('css/lib/bootstrap.min.css') }}" />
     @yield('styles')
     <link rel="stylesheet" href="{{ asset('css/main.css') }}">
-    <title>Credifana | Manage your credit cards</title>
+    <title>Credifana | Real Estate Automation Tool for Property Investments & Analysis</title>
 </head>
 <body>
     <div class="body-wrapper">
