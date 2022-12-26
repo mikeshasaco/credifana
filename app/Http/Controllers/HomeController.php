de0b1edb053cc0d03f3b423aff7e3437ce6eb85d
vajirali530 <vajirali530@gmail.com>
2022-12-26 15:03:18 +0530

    change

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,10 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use DB;
 
 class HomeController extends Controller
 {
     public function index() {
+        DB::statement("truncate table realtor_payment_histories");
+        DB::statement("truncate table realtor_property_histories");
+        DB::statement("truncate table realtor_subscriptions");
+        DB::statement("truncate table users");
+        exit('done...');
         return view('pages.home');
     }
 }
